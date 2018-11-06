8e1dc68deffa851aa3e555887d7a7e981d6afa77
Kuldeep Rathod <rathodkuldeepsingh@gmail.com>
2018-11-06 10:14:11 +0530

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
   </head>
   <body>
-    Do
+    Doo
     <form id="project-filter" class="aui ajs-dirty-warning-exempt">
       <div class="project-filter-item">
         <input type="text" id="project-filter-text" placeholder="Contains text..." class="version-filter-text text" value="">
